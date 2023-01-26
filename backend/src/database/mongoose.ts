0fa5bf86e875e2399ebab78a9d1b4a711f97ce69
genaro <95311127+GenaroMazur@users.noreply.github.com>
2023-01-26 10:44:35 -0300

    Delete backend directory

--- backend/src/database/mongoose.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const mongoosedb  = (url: string) => {
-    mongoose.set('strictQuery', false)
-    mongoose.connect( url, (err) => {
-        if (err) throw err;
-        console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online')
-    })
-}
--- backend/src/helpers/catchAsync.ts
+++
@@ -1,5 +0,0 @@
-import { NextFunction, Request, Response } from "express"
-
-export const catchAsync = (fn: CallableFunction) => (req: Request, res: Response, next: NextFunction) => {
-    fn(req, res, next).catch(next)
-}
